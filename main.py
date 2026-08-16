9ab847b8b3ab67a425a4aca5cb6a5bf04da7780b
Tom Titcombe <hi@tomtitcombe.com>
2026-08-16 20:37:37 +0100

    Message to generate in cli arg
    very fixed setup atm. no chat

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+import argparse
 from pyagi import generate
 
 
-def main() -> None:
-    print(generate("tell me a joke about qwen"))
+def main(message: str) -> None:
+    output = generate(message)
+    print(output)
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(
+                        prog='PyAgi',
+                        description='the power of chatgpt, in the palm of my hand.')
+    parser.add_argument('message')
+    args = parser.parse_args()
+
+    main(args.message)
